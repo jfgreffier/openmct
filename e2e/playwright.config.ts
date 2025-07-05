ba457a6227b062fd087dfcb6e046963d221859c2
jfgreffier <jfgreffier@users.noreply.github.com>
2025-07-05 15:19:24 +0200

    better config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,40 +1,17 @@
-// playwright.config.js
-// @ts-check
+import { defineConfig, devices } from '@playwright/test';
 
-// eslint-disable-next-line no-unused-vars
-import { devices } from '@playwright/test';
-import { fileURLToPath } from 'url';
-const MAX_FAILURES = 5;
-
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-  retries: 2, //Retries 2 times for a total of 3 runs. When running sharded and with max-failures=5, this should ensure that flake is managed without failing the full suite
-  testDir: 'tests',
-  grepInvert: /@mobile/, //Ignore mobile tests
-  testIgnore: '**/*.perf.spec.js', //Ignore performance tests and define in playwright-performance.config.js
-  timeout: 60 * 1000,
-  maxFailures: MAX_FAILURES, //Limits failures to 5 to reduce CI Waste
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  reporter: 'html',
   use: {
     baseURL: 'https://jfgreffier.github.io/openmct/',
-    headless: true,
-    ignoreHTTPSErrors: true,
-    screenshot: 'only-on-failure',
-    trace: 'retain-on-failure',
-    video: 'off',
-    // @ts-ignore - custom configuration option for nyc codecoverage output path
-    coveragePath: fileURLToPath(new URL('../.nyc_output', import.meta.url))
+    trace: 'retain-on-failure'
   },
   projects: [
     {
-      name: 'chrome',
-      testMatch: '**/*.e2e.spec.js', // only run e2e tests
-      grepInvert: /@couchdb|@generatedata/,
-      use: {
-        browserName: 'chromium'
-      }
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
     }
-  ],
-  reporter: 'html'
-};
-
-export default config;
+  ]
+});
